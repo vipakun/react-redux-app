e4c48638c3c4548c9a14b1b5e542ee383a0b4403
Xiangkun Yang <vipakun@hotmail.com>
2016-08-12 12:19:42 -0500

    router and new loader

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,26 +1,12 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-
-// const MyFirstComponent = function () {
-//   return (
-//     <div>
-//       <MyTitle title='whatever' color='rebeccapurple' />
-//       <MyTitle title='LOL' color='papayawhip' />
-//       <MyTitle title='OMG' color='mediumaquamarine' />
-//       <MyTitle title='HelloWorld' color='peru' />
-//       <MyTitle title='Chicago' color='#f06d06' />
-//     </div>
-//   )
-// }
+const Landing = require('./Landing')
+const { Router, Route, hashHistory } = require('react-router')
 
 const App = () => (
-  <div className='app-container'>
-    <div className='home-info'>
-      <h1 className='title'>svideo</h1>
-      <input className='search' type='text' placeholder='Search' />
-      <button className='browser-all'> or Browse All</button>
-    </div>
-  </div>
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
