e5de0a2e62c6920b9d04925d58fff9dd38cc5cf7
Xiangkun Yang <vipakun@hotmail.com>
2016-08-13 09:00:20 -0500

    add state into Search

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,52 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data.json')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((showelement) => (
-        <ShowCard show={showelement} key={showelement.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+// Method #1
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'This is my search term'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm}
+            className='search-input' type='text'
+            placeholder='Search'
+            onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
+// Method #2
+// class Search extends React.Component {
+//   render () {
+//     return (
+//       <div className='container'>
+//         <header className='header'>
+//           <h1 className='brand'>svideo</h1>
+//           <input className='search-input' type='text' placeholder='Search'></input>
+//         </header>
+//         <div className='shows'>
+//           {data.shows.map((show) => (
+//             <ShowCard {...show} key={show.imdbID} />
+//           ))}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 module.exports = Search
