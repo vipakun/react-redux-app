591553cb4bfda0b755c0bd2cd88cb926d9221d8c
Xiangkun Yang <vipakun@hotmail.com>
2016-08-15 12:11:04 -0500

    mount

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,7 +4,7 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
 const ShowCard = require('../js/ShowCard')
-const { shallow } = require('enzyme')
+const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
 
 describe('<Seach />', () => {
@@ -17,5 +17,14 @@ describe('<Seach />', () => {
   it('should render as many shows as there are data for', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
